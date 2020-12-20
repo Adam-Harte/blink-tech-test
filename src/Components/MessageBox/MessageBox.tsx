5b3831a0c6711634b5fe43220b5438e206bacf77
Adam-Harte <adamharte11@hotmail.co.uk>
2020-12-20 21:51:10 +0000

    MessageBox component created to take input for a new message and add it to the selected conversations messages list when send button is pressed

---
+++ src/Components/MessageBox/MessageBox.tsx
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from 'react';
+
+import { ConversationsContext } from '../../Context/ConversationsContext';
+
+import './MessageBox.css';
+
+export const MessageBox: React.FC = () => {
+  const { addMessage, selectedConversation } = useContext(ConversationsContext);
+  const [message, setMessage] = useState({
+    id: '',
+    text: '',
+    last_updated: ''
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.currentTarget;
+
+    setMessage({
+      id: '123gha12',
+      text: value,
+      last_updated: Date.now().toString()
+    });
+  }
+  const handleClick = () => {
+    if (message.text) {
+      addMessage({
+        id: selectedConversation?.id || '',
+        message: {
+          ...message,
+        }
+      });
+    }
+  }
+
+  return (
+    <div className="message-box">
+      <textarea
+        className="message-box__text"
+        value={message.text}
+        onChange={handleChange}
+        placeholder="Enter message"
+        rows={6}
+        cols={70}
+      />
+      <button
+        className="message-box__send-button"
+        type="button"
+        onClick={handleClick}
+      >
+        Send
+      </button>
+    </div>
+  );
+};
+
